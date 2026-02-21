9c15b6e04b3200e0035614e64faa3c903800b9a8
reposcon <cancerbero918@gmail.com>
2026-02-21 10:42:44 -0500

    se corrige error par apoder publicar

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -1,48 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { Role } from '../models/role.model';
+import { Observable } from 'rxjs';
 import { MI_URL_DE_RAILWAY } from '../api-config'; // 1. Importas la base
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class RoleService {
-  // 2. Reemplazas el localhost por la base + /roles
-  private apiUrl = `${MI_URL_DE_RAILWAY}/roles`;
+export class ReportService {
+    private apiUrl = `${MI_URL_DE_RAILWAY}/reports`;
 
-  private rolesSubject = new BehaviorSubject<Role[]>([]);
-  public roles$ = this.rolesSubject.asObservable();
+    constructor(private http: HttpClient) { }
+    
+    checkStatus(): Observable<any> {
+        return this.http.get(`${this.apiUrl}/status`);
+    }
 
-  constructor(private http: HttpClient) { }
+    openSession(amount: number): Observable<any> {
+        return this.http.post(`${this.apiUrl}/open`, { opening_amount: amount });
+    }
 
-  loadRoles(): void {
-    this.http.get<Role[]>(this.apiUrl).subscribe({
-      next: (roles) => this.rolesSubject.next(roles),
-      error: (err) => console.error('Error al cargar roles', err)
-    });
-  }
+    closeSession(): Observable<any> {
+        return this.http.post(`${this.apiUrl}/close`, {});
+    }
 
-  create(role: Partial<Role>): Observable<any> {
-    return this.http.post(this.apiUrl, role).pipe(
-      tap(() => this.loadRoles())
-    );
-  }
-
-  update(id: number, role: Role): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, role).pipe(
-      tap(() => this.loadRoles())
-    );
-  }
-
-  delete(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`).pipe(
-      tap(() => this.loadRoles())
-    );
-  }
-
-  get currentRolesValue(): Role[] {
-    return this.rolesSubject.value;
-  }
+    getHistory(): Observable<any> {
+        return this.http.get(`${this.apiUrl}/history`);
+    }
 }
