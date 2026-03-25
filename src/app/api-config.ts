05ca1813e0027a4d7f1421a6f91633d3eb09d8e0
reposcon <cancerbero918@gmail.com>
2026-03-25 15:15:48 -0500

    cambio de host

--- src/app/api-config.ts
+++ src/app/api-config.ts
@@ -1,2 +1,2 @@
-export const MI_URL_DE_RAILWAY = 'https://programa-de-gestion-basico-api-production.up.railway.app/api';
-// export const MI_URL_DE_RAILWAY = 'http://127.0.0.1:8000/api';
+ export const MI_URL_DE_RAILWAY = 'https://web-production-07f8e.up.railway.app/';
+//export const MI_URL_DE_RAILWAY = 'http://127.0.0.1:8000/api';
